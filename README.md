3c5e76e8404aaad9f2173e4a4f69ad5e2b050476
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2026-01-03 22:40:15 +0530

    setup treesitter

---
+++ README.md
@@ -0,0 +1,8 @@
+Intro
+Explain vim opt and keybindings
+Explain lua code with lazy
+Explain treesitter
+- old vim used regex to syntax highlight but treesitter parses the text and makes a syntax tree that then is native to the lanugage code for better syntax highlighting
+Common syntax and practises and boiler plate code in nvim config plugins 
+
+
--- init.lua
+++ init.lua
@@ -1,32 +1,32 @@
+-- relative line numbers and line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-
+--annoying line wrap
 vim.opt.wrap = false
-
+-- 4 spaces for tabs than 8
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
+-- clipboard sync
 vim.opt.clipboard = "unnamedplus"
-
+-- when typing cmds ignore case
 vim.opt.ignorecase = true
-
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--this says either vim.uv or vim.loop
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -40,21 +40,62 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
-
--- Colorscheme
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    require('onedark').load()
-  end
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({ style = "darker" })
+      require("onedark").load()
+    end,
   },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+--  config = function()
+--     local ok, configs = pcall(require, "nvim-treesitter.configs")
+--     if not ok then
+--       return
+--     end
+-- 
+--     configs.setup({
+--       ensure_installed = { "lua" },
+--       highlight = { enable = true },
+--       indent = { enable = true },
+--     })
+--   end,
+  },
+},
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+-- keymaps
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Visual mode mapping for commenting lines for lua  quick fix
+vim.api.nvim_set_keymap(
+  "v",
+  "<leader>/",
+  ":s/^/-- /<CR>:noh<CR>",
+  { noremap = true, silent = true }
+)
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>wq", ":wq<CR>")
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
 
