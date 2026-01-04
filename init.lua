7c88aa0b810ea408fd91541929fda7c29f216d0e
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2026-01-04 16:18:05 +0530

    treesitter bug fix

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Onvim
 -- relative line numbers and line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -17,7 +18,8 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 --this says either vim.uv or vim.loop
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
+    lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -35,44 +37,72 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+ require("lazy").setup(
+	 {
+         {
+             "navarasu/onedark.nvim",
+             priority = 1000,
+             config = function()
+                 require("onedark").setup({ style = "darker" })
+                 require("onedark").load()
+             end,
+         },
+         {
+             "nvim-treesitter/nvim-treesitter",
+             build = ":TSUpdate",
+             lazy = true,              -- don't load immediately
+             event = "BufReadPost",
+             config = function()
+                 require("nvim-treesitter.config").setup({
+                     ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+                     auto_install = true,
+                     highlight = { enable = true },
+                 })
+             end,
+         },
+ },
+   -- Configure any other settings here. See the documentation for more details.
+   -- colorscheme that will be used when installing plugins.
+   { 
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+    }
+ )
+ 
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    config = function()
-      require("onedark").setup({ style = "darker" })
-      require("onedark").load()
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
---  config = function()
---     local ok, configs = pcall(require, "nvim-treesitter.configs")
---     if not ok then
---       return
---     end
+-- require("lazy").setup(
+--   {
+--     -- Plugin list (directly)
+--     {
+--       "navarasu/onedark.nvim",
+--       priority = 1000,
+--       config = function()
+--         require("onedark").setup({ style = "darker" })
+--         require("onedark").load()
+--       end,
+--     },
+--     {
+--       "nvim-treesitter/nvim-treesitter",
+--       build = ":TSUpdate",
+--       lazy = true,
+--       event = "BufReadPost",
+--       config = function()
+--         require("nvim-treesitter.config").setup({
+--           ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+--           auto_install = true,
+--           highlight = { enable = true },
+--         })
+--       end,
+--     },
+--   },
 -- 
---     configs.setup({
---       ensure_installed = { "lua" },
---       highlight = { enable = true },
---       indent = { enable = true },
---     })
---   end,
-  },
-},
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
-
+--   {
+--     -- Global Lazy.nvim options
+--     install = { colorscheme = { "habamax" } },
+--     checker = { enabled = true },
+--   }
+-- )
 -- keymaps
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
